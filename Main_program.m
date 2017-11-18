f494c9b645d575a35799ffebda1aedb2a112a990
M. Serdar Karaman <Mavrikant@users.noreply.github.com>
2017-11-18 21:34:43 +0300

    +shadow effect +plot connected users

--- Main_program.m
+++ Main_program.m
@@ -4,33 +4,34 @@ close all;
 
 % Code Style Guide
 % 1 - User _ in variable names
-% 2 - Use space before and after +,-,=
-% 3 - Use space after ,
+% 2 - All variables start with capital letter
+% 3 - Use space before and after +, -, =, >, <
+% 4 - Use space after ,
 %
 %
 
 
-Fc = 1800; % Carrier freq (Mhz)
+Fc = 1800; % Carrier freq (Mhz) (for LTE in Turkey: 800, 900, 1800, 2100, 2600(only indoor femtocell) 
 H_bts = 40; % Height of BTS
-User_number = 1000;
-
-Tx_power = 46; % BTS power (dBm) 
+UE_number = 1000; % User equipment (phone, tablet ...) number
+Threshold= -200 ; % UE connection threshold (Db)
+Tx_power = 46; % BTS power (dBm)  
 Tx_a_gain = 18; % Anten gain (dBi)
 Tx_c_loss = 2.5; % Anten  cable loss (dB)
 Tx_EiRP = Tx_power + Tx_a_gain - Tx_c_loss;% Effective isotropic radiated power (dBm)
 
-User_database=zeros(User_number,4); % Matrix for user positions other user related informations
+UE_database=zeros(UE_number,4); % Matrix for user positions other user related informations
 
-User_database(:,1) = randi([-15000,15000], 1, User_number); %generate random user position X (meter)
-User_database(:,2) = randi([-15000,15000], 1, User_number); %generate random user position Y (meter)
-User_database(:,3) = sqrt(User_database(:,1).*User_database(:,1) + User_database(:,2).*User_database(:,2)); % Calculated distane between user and BTS (meter)
+UE_database(:,1) = randi([-15000,15000], 1, UE_number); %generate random user position X (meter)
+UE_database(:,2) = randi([-15000,15000], 1, UE_number); %generate random user position Y (meter)
+UE_database(:,3) = sqrt(UE_database(:,1).*UE_database(:,1) + UE_database(:,2).*UE_database(:,2)); % Calculated distane between user and BTS (meter)
 
 %%%%%%%%
 % Okumura-Hata Model model for open area (Developed by Masaharu Hata after Okumura)
 % A = 69.55 + 26.16*log10(Fc) - 13.82*log10(hb);
 % B = 44.9 - 6.55*log10(hb);
 % D = 4.78*log10(Fc)^2 + 18.33*log10(Fc) + 40.94;
-% Pathloss_formula = A + B*log10(User_database(:,3)) - D; 
+% Pathloss_formula = A + B*log10(UE_database(:,3)) - D; 
 
 %%%%%%%%%%%%%%%
 % Cost-231 Model (also known as COST-Hata-Model) 
@@ -38,16 +39,29 @@ User_database(:,3) = sqrt(User_database(:,1).*User_database(:,1) + User_database
 A = 46.3 + 33.9*log10(Fc) - 13.82*log10(H_bts); % a(hm) need to be clarified
 B = 44.9 - 6.55*log10(H_bts);
 C = 0; % 0 for medium-seze city and suburban; 3 for metropolitancenters
-Pathloss_formula = A + B*log10(User_database(:,3)) + C;  %(dB)
+Pathloss_formula = A + B*log10(UE_database(:,3)) + C;  %(dB)
+UE_database(:,4) = Tx_EiRP - Pathloss_formula - 8*randn(UE_number,1);% Recieved power by user (BTS power - Pathloss - Shadowing)
 
-figure 1;
-  distance=linspace(0,20000,1000); % 0 -20Km, 1K sample
-  title(['Cost-231 Model, Fc=' num2str(Fc) ', H_bts=' num2str(H_bts)])
-  plot(distance,A + B*log10(distance) + C)
-  xlabel('Distance in meter')
-  ylabel('Pathloss in Db')
+figure (1);
+    Distance = linspace(0,20000,1000); % 0 -20Km, 1K sample
+    plot(Distance, A + B*log10(Distance) + C)
+    title(['Cost-231 Model, Fc=' num2str(Fc) ', H-bts=' num2str(H_bts)])
+    xlabel('Distance in meter')
+    ylabel('Pathloss in Db')
 
-User_database(:,4) = Tx_EiRP - Pathloss_formula; % Recieved power by user
 
-figure 2;
-  scatter(User_database(:,1),User_database(:,2)); %%plot user positions
+Connected_users = find(UE_database(:,4) > Threshold); % Find connected UE_id numbers
+
+figure (2);
+    plot(0, 0, 'Ob', 'LineWidth', 3);%BTS at center
+    title(['Cost-231 Model, Fc=' num2str(Fc) ', H-bts=' num2str(H_bts) ', threshold=' num2str(Threshold)])
+    hold on;
+    for i=1:UE_number
+        if (~isempty(find(Connected_users==i, 1))) == 0
+            plot(UE_database(i,1), UE_database(i,2), '.r'); % Connected
+            hold on;
+        else
+            plot(UE_database(i,1), UE_database(i,2), 'og'); % NOT connected
+            hold on;
+        end 
+    end
