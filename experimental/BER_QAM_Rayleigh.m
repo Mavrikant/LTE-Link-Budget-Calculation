c71cdb32b0f09ba971efb8b0a26dfe62eb834f89
M. Serdar Karaman <Mavrikant@users.noreply.github.com>
2017-12-01 22:09:57 +0300

    Theoretical BER of M-QAM in Rayleigh Fading

---
+++ experimental/BER_QAM_Rayleigh.m
@@ -0,0 +1,23 @@
+%http://www.raymaps.com/index.php/theoretical-ber-of-m-qam-in-rayleigh-fading/
+clc;
+clear variables;
+close all;
+
+EbNo = linspace(0,60,200);
+four_QAM = 1/2 * ( 1 - sqrt(10.^(EbNo/10)./(1+10.^(EbNo/10))) );
+sixteen_QAM = 3/8 * ( 1 - sqrt(2/5*10.^(EbNo/10)./(1+2/5*10.^(EbNo/10))) );
+sixty_four_QAM = 7/24 * ( 1 - sqrt(1/7*10.^(EbNo/10)./(1+1/7*10.^(EbNo/10))) );
+
+figure (1)
+    semilogy(EbNo,four_QAM)
+    hold on;
+    semilogy(EbNo,sixteen_QAM)
+    hold on;
+    semilogy(EbNo,sixty_four_QAM)
+    hold on;
+    axis([-inf +inf 0.000001 1])
+    legend('4QAM','16QAM','64QAM')
+    grid on;
+    title('Theoretical BER of M-QAM in Rayleigh Fading')
+    xlabel('Eb/No (dB)')
+    ylabel('BER')
