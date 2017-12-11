b3ba4a694be39fdb243b41a21d6fe38873df1f99
M. Serdar Karaman <Mavrikant@users.noreply.github.com>
2017-12-11 21:16:16 +0300

    Update Main_Rural.m

--- Main_Rural.m
+++ Main_Rural.m
@@ -2,32 +2,25 @@ clc;
 clear variables;
 close all;
 
-x0=0;
-y0=0;
-b=zeros(1,75);
-UE_density = 10; %Active user density (/km2)
 Fc = 900; % Carrier freq (Mhz) (for LTE in Turkey: 800, 900, 1800, 2100, 2600(only indoor femtocell) 
-Cell_size = 15000; % Macro cell (m)
-UE_number = floor(UE_density*(Cell_size^2/10^6)); % User equipment (phone, tablet ...) number
+Cell_size = 20000; % Micro cell (m)
 H_bts = 30; % Height of BTS; http://ftp.tiaonline.org/TR-8/TR-8.18.4/Working/WG4-8.18.4_16-05-039-R6%20LTE%20Transmitter%20Characteristics.pdf
 H_ue = 3; % Height of user equipment
-Threshold_voiceqpsk= -118 ; % UE connection Threshold_voice (Db)
-Threshold_dataqpsk= -90 ; % UE connection Threshold_data (Db)
-Data_UE_rate = 0.2; % Ratio of data requests by UE
-Tx_power = 43; % BTS power (dBm)  
-Tx_a_gain = 18; % Antenna gain (dBi)
+UE_density = 10; %Active user density (/km2)
+UE_number = floor(UE_density*(4*Cell_size^2/10^6)); % User equipment (phone, tablet ...) number
+Threshold_voice= Rec_sens(1,1) ; % UE connection Threshold_voice (dB)
+Threshold_data= Rec_sens(1,2) ; % UE connection Threshold_data (dB)
+Data_UE_rate = 0.4; % Ratio of data requests by UE
+Tx_power = 40; % BTS power (dBm)  
+Tx_a_gain = 18; % Antenna gain (dBi) 
 Tx_c_loss = 2.5; % Antenna  cable loss (dB)
 Rx_body_loss = 3; % Body loss at reciever side (dB) (User Body Loss Study for Popular Smartphones - http://ieeexplore.ieee.org/stamp/stamp.jsp?arnumber=7228963)
 Tx_EiRP = Tx_power + Tx_a_gain - Tx_c_loss; % Effective isotropic radiated power (dBm)
 
 %% Create UE positions and their distances
 UE_database=zeros(UE_number,5); % Matrix for UE positions other UE related informations
-angle1 = 0;
-angle2 = 2*pi;
-t=(angle2 - angle1) * rand(UE_number,1) + angle1;
-r = Cell_size*sqrt(rand(UE_number,1));
-UE_database(:,1) = x0 + r.*cos(t);
-UE_database(:,2) = y0 + r.*sin(t);
+UE_database(:,1) = randi([-Cell_size,Cell_size], 1, UE_number); % Create random UE position X (meter)
+UE_database(:,2) = randi([-Cell_size,Cell_size], 1, UE_number); % Create random UE position Y (meter)
 UE_database(:,3) = sqrt(UE_database(:,1).^2 + UE_database(:,2).^2); % Calculated distance between UE and BTS (meter)
 UE_database(:,5) = rand(UE_number,1) < Data_UE_rate; % User type (1=Data, 0=Voice)
 
@@ -44,18 +37,18 @@ UE_database(:,4) = Tx_EiRP - Pathloss_formula - Shadowing_eff - Rx_body_loss ;% 
 %% Draw UE positions and their status on map
 figure (1);
     plot(0, 0, '*k', 'LineWidth', 3); % BTS at center
-    title(['RURAL, HATA Model, Fc=' num2str(Fc) 'Mhz, H-bts=' num2str(H_bts) 'm, Tx-power=' num2str(Tx_power) 'dB, Threshold-voice=' num2str(Threshold_voiceqpsk) 'dB'])
+    title(['RURAL, HATA Model, Fc=' num2str(Fc) 'Mhz, H-bts=' num2str(H_bts) 'm, Tx-power=' num2str(Tx_power) 'dB, Threshold-voice=' num2str(Threshold_voice) 'dB'])
     hold on;
     grid on;
     for i=1:UE_number
         if UE_database(i,5) %Data
-            if UE_database(i,4) > Threshold_dataqpsk %Connected
+            if UE_database(i,4) > Threshold_data %Connected
                 plot(UE_database(i,1), UE_database(i,2), '*g'); 
             else % NOT connected
                 plot(UE_database(i,1), UE_database(i,2), '*r'); 
             end
         else %Voice
-            if UE_database(i,4) > Threshold_voiceqpsk %Connected
+            if UE_database(i,4) > Threshold_voice %Connected
                 plot(UE_database(i,1), UE_database(i,2), '.g'); 
             else % NOT connected
                 plot(UE_database(i,1), UE_database(i,2), '.r'); 
@@ -63,20 +56,11 @@ figure (1);
         end 
     end
     
-%% Pathloss vs Distance graph for Cost-231 model
-figure (2);
-    Distance = linspace(0,Cell_size,2250); % 0-Cell_size Km, 1K sample
-    plot(Distance, sort(Pathloss_formula)) % Distance in Km
-    title({'Pathloss vs Distance (HATA)';['RURAL, HATA Model, Fc=' num2str(Fc) 'Mhz, H-bts=' num2str(H_bts) 'm, Tx-power=' num2str(Tx_power) 'dB, Threshold-voice=' num2str(Threshold_voiceqpsk) 'dB']})
-    xlabel('Distance in meter')
-    ylabel('Pathloss in dB')
-    clear Distance
-    grid on
-    
+
 %% Cumulative distribution of received power
     figure (3);
         ecdf(UE_database(:,4))
-        title({'Cumulative distribution of received power';['RURAL, HATA Model, Fc=' num2str(Fc) 'Mhz, H-bts=' num2str(H_bts) 'm, Tx-power=' num2str(Tx_power) 'dB, Threshold-voice=' num2str(Threshold_voiceqpsk) 'dB']})
+        title({'Cumulative distribution of received power';['RURAL, HATA Model, Fc=' num2str(Fc) 'Mhz, H-bts=' num2str(H_bts) 'm, Tx-power=' num2str(Tx_power) 'dB, Threshold-voice=' num2str(Threshold_voice) 'dB']})
         xlabel('Received power (dB)')
         ylabel('Probability')
         grid on;
@@ -84,95 +68,48 @@ figure (2);
 %% Histogram of received power by UE   
     figure (4);
         histogram(UE_database(:,4));
-        title({'Histogram of received power by UE';['RURAL, HATA Model, Fc=' num2str(Fc) 'Mhz, H-bts=' num2str(H_bts) 'm, Tx-power=' num2str(Tx_power) 'dB, Threshold-voice=' num2str(Threshold_voiceqpsk) 'dB']})
+        title({'Histogram of received power by UE';['RURAL, HATA Model, Fc=' num2str(Fc) 'Mhz, H-bts=' num2str(H_bts) 'm, Tx-power=' num2str(Tx_power) 'dB, Threshold-voice=' num2str(Threshold_voice) 'dB']})
         xlabel('Received power (dB)');
         ylabel('Count');
         grid on;
      
-%% Coverage graph
+%% Coverage graph voice
 Sample_size = 1000;       
-Distance = linspace(0, Cell_size, Sample_size); % 0-15Km, 1K sample
+Distance = linspace(0, Cell_size, Sample_size); % 0-2Km, 1K sample
 Coverage = zeros(1, Sample_size);
-for i=1:1000
-    Pathloss_formula =  A + B*log10((Distance(i)./1000)*ones(Sample_size, 1)) - D;  % Distance in Km; (Sample_size, 1) matrix
-    Shadowing_eff = normrnd (0, 8, [Sample_size, 1] ); % (Sample_size, 1) matrix
-    Power = Tx_EiRP - Pathloss_formula - Shadowing_eff - Rx_body_loss ; % Received power by UE 
-    Coverage(i) = length(find(Power > Threshold_voiceqpsk))/Sample_size; % Find connected UE number
+for i=1:Sample_size
+    Pathloss_formula = A + B*log10((Distance(i)./1000)) - D; % Distance in Km; (Sample_size, 1) matrix
+    Shadowing_eff = normrnd (0, sqrt(8), [Sample_size, 1] ); % (Sample_size, 1) matrix
+    Power = Tx_EiRP - Pathloss_formula - Shadowing_eff - Rx_body_loss; % Received power by UE 
+    Coverage(i) = length(find(Power > Threshold_voice))/Sample_size; % Find connected UE number
 end
 clear Power Pathloss_formula Shadowing_eff Sample_size
 figure (5);
-    plot (Distance, smooth(Coverage)); % Moving average smoothing
-    axis([-inf +inf 0 1]); % fix y axis between 0 and 1
-    title({'Coverage probability vs Distance';['RURAL, HATA Model, Fc=' num2str(Fc) 'Mhz, H-bts=' num2str(H_bts) 'm, Tx-power=' num2str(Tx_power) 'dB, Threshold-voice=' num2str(Threshold_voiceqpsk) 'dB']})
+    plot (Distance, smooth(Coverage)) % Moving average smoothing
+    axis([-inf +inf 0 1]) % fix y axis between 0 and 1
+    grid on;
+    title({'Coverage probability vs Distance (voice)';['RURAL, Hata Model, Fc=' num2str(Fc) 'Mhz, H-bts=' num2str(H_bts) 'm, Tx-power=' num2str(Tx_power) 'dB, Threshold-voice=' num2str(Threshold_voice) 'dB']});
     xlabel('Distance (m)');
     ylabel('Probability');
-    grid on;
-
+    
+    
+%% Coverage graph data
 Sample_size = 1000;       
-Distance = linspace(0, Cell_size, Sample_size); % 0-15Km, 1K sample
+Distance = linspace(0, Cell_size, Sample_size); % 0-2Km, 1K sample
 Coverage = zeros(1, Sample_size);
-for i=1:1000
-    Pathloss_formula =  A + B*log10((Distance(i)./1000)*ones(Sample_size, 1)) - D;  % Distance in Km; (Sample_size, 1) matrix
-    Shadowing_eff = normrnd (0, 8, [Sample_size, 1] ); % (Sample_size, 1) matrix
-    Power = Tx_EiRP - Pathloss_formula - Shadowing_eff - Rx_body_loss ; % Received power by UE 
-    Coverage(i) = length(find(Power > Threshold_dataqpsk))/Sample_size; % Find connected UE number
+for i=1:Sample_size
+    Pathloss_formula = A + B*log10((Distance(i)./1000)) - D; % Distance in Km; (Sample_size, 1) matrix
+    Shadowing_eff = normrnd (0, sqrt(8), [Sample_size, 1] ); % (Sample_size, 1) matrix
+    Power = Tx_EiRP - Pathloss_formula - Shadowing_eff - Rx_body_loss; % Received power by UE 
+    Coverage(i) = length(find(Power > Threshold_data))/Sample_size; % Find connected UE number
 end
 clear Power Pathloss_formula Shadowing_eff Sample_size
 figure (6);
-    plot (Distance, smooth(Coverage)); % Moving average smoothing
-    axis([-inf +inf 0 1]); % fix y axis between 0 and 1
-    title({'Coverage probability vs Distance';['RURAL, HATA Model, Fc=' num2str(Fc) 'Mhz, H-bts=' num2str(H_bts) 'm, Tx-power=' num2str(Tx_power) 'dB, Threshold-dataqpsk=' num2str(Threshold_voiceqpsk) 'dB']})
-    xlabel('Distance (m)');
-    ylabel('Probability');
+    plot (Distance, smooth(Coverage)) % Moving average smoothing
+    axis([-inf +inf 0 1]) % fix y axis between 0 and 1
     grid on;
-    
-Threshold_data64qam=-78;
-Sample_size = 1000;       
-Distance = linspace(0, Cell_size, Sample_size); % 0-15Km, 1K sample
-Coverage1 = zeros(1, Sample_size);
-for i=1:1000
-    Pathloss_formula =  A + B*log10((Distance(i)./1000)*ones(Sample_size, 1)) - D;  % Distance in Km; (Sample_size, 1) matrix
-    Shadowing_eff = normrnd (0, 8, [Sample_size, 1] ); % (Sample_size, 1) matrix
-    Power = Tx_EiRP - Pathloss_formula - Shadowing_eff - Rx_body_loss ; % Received power by UE 
-    Coverage1(i) = length(find(Power > Threshold_data64qam))/Sample_size; % Find connected UE number
-end
-clear Power Pathloss_formula Shadowing_eff Sample_size
-figure (7);
-    plot (Distance, smooth(Coverage1)); % Moving average smoothing
-    axis([-inf +inf 0 1]); % fix y axis between 0 and 1
-    title({'Coverage probability vs Distance';['RURAL, HATA Model, Fc=' num2str(Fc) 'Mhz, H-bts=' num2str(H_bts) 'm, Tx-power=' num2str(Tx_power) 'dB, Threshold-data 64QAM=' num2str(Threshold_data64qam) 'dB']})
+    title({'Coverage probability vs Distance (data)';['RURAL, Hata Model, Fc=' num2str(Fc) 'Mhz, H-bts=' num2str(H_bts) 'm, Tx-power=' num2str(Tx_power) 'dB, Threshold-data=' num2str(Threshold_data) 'dB']});
     xlabel('Distance (m)');
-    ylabel('Probability');
-    grid on;
+    ylabel('Probability'); 
     
-Fc2=1500;
-A2 = 69.55 + 26.16*log10(Fc2) - 13.82*log10(H_bts) - ahm;
-B2 = 44.9 - 6.55*log10(H_bts);
-D2 = 4.78*log10(Fc2)^2 - 18.33*log10(Fc2) + 40.94;
-Pathloss_formula2 = A2 + B2*log10(UE_database(:,3)/1000) - D2;  %(dB) (distance in Km)
-
-figure (8);
-    Distance = linspace(0,Cell_size,2250); % 0-Cell_size Km, 1K sample
-    plot(Distance, sort(Pathloss_formula2)) % Distance in Km
-    title({'Pathloss vs Distance (HATA)';['RURAL, HATA Model, Fc=' num2str(Fc2) 'Mhz, H-bts=' num2str(H_bts) 'm, Tx-power=' num2str(Tx_power) 'dB, Threshold-voice=' num2str(Threshold_voiceqpsk) 'dB']})
-    xlabel('Distance in meter')
-    ylabel('Pathloss in dB')
-    clear Distance
-    grid on
     
-ahm2 = 3.2*(log10(1.75*H_ue))^2 - 4.97; % for large cities
-A3 = 46.3 + 33.9*log10(Fc2) - 13.82*log10(H_bts) - ahm; 
-B3 = 44.9 - 6.55*log10(H_bts);
-C3 = 3; % 0 for medium-size city and suburban; 3 for metropolitan centers
-Pathloss_formula3 = A3 + B3*log10(UE_database(:,3)/1000) + C3; % (dB) (distance in Km)
-
-figure (9);
-    Distance = linspace(0,Cell_size,2250); % 0-Cell_size Km, 1K sample
-    plot(Distance, sort(Pathloss_formula3)) % Distance in Km
-    title({'Pathloss vs Distance (COST 231)';['RURAL, COST 231 Model, Fc=' num2str(Fc2) 'Mhz, H-bts=' num2str(H_bts) 'm, Tx-power=' num2str(Tx_power) 'dB, Threshold-voice=' num2str(Threshold_voiceqpsk) 'dB']})
-    xlabel('Distance in meter')
-    ylabel('Pathloss in dB')
-    clear Distance
-    grid on
-
-
